667b211126fe08a3453e3be6f4753db86f645a9a
jjeerryyy28 <prajwalnikhar1@gmail.com>
2024-01-29 03:56:34 +0530

    react csv table openinApp assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Upload from "./Components/Upload/upload";
+import SignIn from "./Components/SignIn/signin";
+import Register from "./Components/SignIn/Register";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/error" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
-}
+};
+
+// Create a NotFound component to handle unknown paths
+const NotFound = () => {
+  return <div>Page not found</div>;
+};
 
 export default App;
